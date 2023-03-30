03fe91b3c64b49c8f0b4d5928c2bcdd58b30fbbb
oscar91511 <gamingsoftwarepereira@gmail.com>
2023-03-30 13:50:11 -0500

    change on products whit desption to longers, cut some text

--- assets/json/products.js
+++ assets/json/products.js
@@ -1,7 +1,7 @@
 const items = [
     {
         "id": 1,
-        "name": "Ryzen 7 1700(8 nucleos, 16hilos,3 Ghz a 3.7 GHZ)",
+        "name": "Ryzen 7 1700(8 nucleos)",
         "price": 950000,
         "quantity": 3,
         "image": "https://www.info-computer.com/modules/dbblog/views/img/uploads/2020/04/ryzen-1700-768x524.jpg"
@@ -9,7 +9,7 @@ const items = [
     
     {
         "id": 2,
-        "name": "Intel-i3 10100/B560M-A/8gbs ram/500gb/morovol white",
+        "name": "Intel-i3 10100/morovol white",
         "price": 1500000,
         "quantity": 4,
         "image": "https://i.ibb.co/CvM21P6/img3.jpg"
@@ -17,7 +17,7 @@ const items = [
 
     {
         "id": 3,
-        "name": "Intel-i5 11400/RTX 2060/B560M-A/16gbs ram/1tb/thermaltakeV200TG",
+        "name": "Intel-i5 11400/RTX 2060/thermaltakeV200TG",
         "price": 5100000,
         "quantity": 5,
         "image": "https://i.ibb.co/W61HVdb/img1.jpg"
@@ -25,7 +25,7 @@ const items = [
 
     {
         "id": 4,
-        "name": "Intel-i3 12100/16gb/Cweb/monitor",
+        "name": "Intel-i3 12100/Cweb/monitor",
         "price": 3000000,
         "quantity": 3,
         "image": "https://i.ibb.co/JCVmTF1/img2.jpg"
@@ -34,7 +34,7 @@ const items = [
 
     {
         "id": 5,
-        "name": "Ryzen 7 5800x/Rtx 3070/32gb ram Rgb/B550 Gaming Plus",
+        "name": "Ryzen 7 5800x/Rtx 3070",
         "price": 9000000,
         "quantity": 5,
         "image": "https://i.ibb.co/Df7vGyW/img5.jpg"
@@ -50,7 +50,7 @@ const items = [
 
     {
         "id": 7,
-        "name": "Intel I5 11400|Gtx 1660|8gbs ram|500gb",
+        "name": "Intel I5 11400|Gtx 1660",
         "price": 3200000,
         "quantity": 3,
         "image": "https://i.ibb.co/pKR4Jby/img7.jpg"
@@ -58,7 +58,7 @@ const items = [
 
     {
         "id": 8,
-        "name": "ryzen5 5600X|rtx3060|16gb ram rgb|x2 1tb m.2",
+        "name": "ryzen5 5600X|rtx3060",
         "price": 5928000,
         "quantity": 3,
         "image": "https://i.ibb.co/tJnSfSc/img8.jpg"
@@ -66,7 +66,7 @@ const items = [
 
     {
         "id": 9,
-        "name": "ryzen 5500|Gtx 1660 SUPER|16GB ram1|256gb+500gb",
+        "name": "ryzen 5500|Gtx 1660 SUPER",
         "price": 4500000,
         "quantity": 3,
         "image": "https://i.ibb.co/njKfHqF/img9.jpg"
